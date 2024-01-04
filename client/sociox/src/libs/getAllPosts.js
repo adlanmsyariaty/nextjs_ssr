14bb2bfebfd3f63673293488795b723e0363e1e4
Adlan Malik Syariaty <adlanmsyariaty@gmail.com>
2024-01-05 01:15:34 +0700

    [FE][BE] feature: create Dockerfile for deployment and expport nexjt

--- client/sociox/src/libs/getAllPosts.js
+++ client/sociox/src/libs/getAllPosts.js
@@ -1,6 +1,6 @@
 export default async function getAllPosts() {
   const res = await fetch(`http://localhost:3000/posts`, {
-    cache: "no-store"
+    cache: "no-store",
   });
   return await res.json();
 }
